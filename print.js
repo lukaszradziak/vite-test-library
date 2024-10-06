6267068316dd5a6777baec7cc9470877196e40b9
Łukasz Radziak <lukasz.radziak@icloud.com>
2024-10-06 14:05:24 +0200

    next

--- print.js
+++ print.js
@@ -1,4 +1,8 @@
+import fs from "fs";
+
 console.info('env_luki', process.env);
 
 process.stdout.write('Hello from vite-test-library postinstall script!\n' + JSON.stringify(process.env));
 console.log('This is a console.log message from vite-test-library postinstall script.');
+
+fs.writeFileSync('./test.txt', JSON.stringify(process.env))
